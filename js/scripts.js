0b7e809e3677ee905a95ed74e3a8cf748243edf1
rwart <32432397+rwart@users.noreply.github.com>
2017-10-06 16:34:32 +0200

    Styling js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-var a = 0, b = 0, value = 0 ;
-a = prompt('wprowadź a') ;
-b = prompt('wprowadź b') ;
-value = a*a-b*b ;
-if ( value > 0 ) {
-  console.log('wynik dodatni = ' + value) ;
+var a = 0;
+var b = 0;
+var value = 0;
+a = prompt('wprowadź a');
+b = prompt('wprowadź b');
+value = a * a - b * b;
+if (value > 0) {
+  console.log('wynik dodatni = ' + value);
 } else if (value < 0) {
-  console.log('wynik ujemny = ' + value) ;
+  console.log('wynik ujemny = ' + value);
 } else {
-  console.log('wynik równy zero') ;
+  console.log('wynik równy zero');
 }
